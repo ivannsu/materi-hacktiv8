214ef4713c4a3a477ba60e4bc9e2a0648441af7d
ivan <ivansu.webmail@gmail.com>
2018-07-24 10:06:53 +0700

    Initialize graduates.js

---
+++ week5/projects-1/graduates.js
@@ -0,0 +1,35 @@
+function graduates(students) {
+
+}
+
+console.log(graduates([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+// {
+//  foxes: [
+//    { name: 'Dimitri', score: 90 }
+//  ],
+//  wolves: [
+//    { name: 'Alexei' , score: 85 },
+//    { name: 'Anastasia', score: 78 }
+//  ]
+// }
