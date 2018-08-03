0eb6e60efffb5dd8b1bc141626b5a9a063df4cc0
ivan <ivansu.webmail@gmail.com>
2018-08-03 22:55:09 +0700

    Pemindahan direktori QUIZ ke github pages

--- week6/quiz/assets/js/questions.js
+++
@@ -1,9 +0,0 @@
-function Question(question, options, answer) {
-  this.question = question;
-  this.options = options;
-  this.answer = answer;
-}
-
-Question.prototype.checkAnswer = function(choice) {
-  return (this.answer === choice) ? true : false;
-}
